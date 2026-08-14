6476916228818ac73c0cba040fa3ac20912029f4
Kenji Ishikawa <ke.oqzl@gmail.com>
2026-08-14 10:05:49 +0900

    Remove merged lifecycle helper

--- Sources/CameraGeometryKit/Depth/CameraSynchronizedFrameLifecycle.swift
+++
@@ -1,7 +0,0 @@
-import AVFoundation
-
-extension CameraDepthDelivery {
-    func setCameraPosition(_ position: AVCaptureDevice.Position) {
-        lock.withLock { cameraPosition = CameraPosition(position) }
-    }
-}
