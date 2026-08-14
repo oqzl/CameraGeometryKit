84ebe79f15665796f9679ebd91473d296a5d783e
Kenji Ishikawa <ke.oqzl@gmail.com>
2026-08-14 09:45:38 +0900

    Retire merged depth callback extension

--- Sources/CameraGeometryKit/Capture/CameraDepthDeliveryCallback.swift
+++ Sources/CameraGeometryKit/Capture/CameraDepthDeliveryCallback.swift
@@ -1,10 +1,1 @@
-@preconcurrency import AVFoundation
-
-extension CameraDepthDelivery {
-    func dataOutputSynchronizer(
-        _ synchronizer: AVCaptureDataOutputSynchronizer,
-        didOutput collection: AVCaptureSynchronizedDataCollection
-    ) {
-        deliver(collection)
-    }
-}
+// Merged into CameraDepthDelivery.swift.
